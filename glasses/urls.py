c5a19b474187d24d46f5f49865abb802bca4053a
Nazar <rednaz1990@gmail.com>
2018-02-02 22:08:46 +0200

    install debug_toolbar, replace static files

--- glasses/urls.py
+++ glasses/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
 from . import views
 
@@ -21,3 +22,4 @@ urlpatterns = [
     url(r'^list/', views.glasses_list, name='list'),
     url(r'^add/$', views.glasses_add, name='add'),
 ]
+
--- optick/settings.py
+++ optick/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'debug_toolbar',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+INTERNAL_IPS = '127.0.0.1'
